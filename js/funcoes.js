256b0e12bd2669203e952652ebd9bc10e8dff9c2
anacardozo <105667369+anacardozo@users.noreply.github.com>
2022-12-10 00:45:02 -0300

    v3

--- js/funcoes.js
+++ js/funcoes.js
@@ -16,38 +16,41 @@ function texto1(){
            x[i].getElementsByTagName("texto1")[0].childNodes[0].nodeValue);
     }
 }
-function blog(){
-   for(i=y.length;i>0;i++){
+function noticiageral(){
+   for(i=y.length-1;i>=0;i--){
     document.write(
-        "<div class='card'>"+
-        "<img class='card-img-top' src='imgs/"+y[i].getElementsByTagName(imagem)[0].childNodes[0].nodeValue +"'width:'400px'></imgs>"+
+        "<div class='card' id='cartao' style='width:400px' >"+
+          "<div class='card-body'>"+
+        "<div class='card position-relative'>"+
+        "<img class='card-img-top' src='imgs/"+y[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue +"'width='400px'>"+
         "<div class='card-body'>"+
-        [i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</h4>"+
+        "<h4>"+
+        y[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +"</h4>"+
         "</div>"+
         "<div class='card-body'>"+
-        "<p class='card-text'>"+y[i].getElementsByTagName("texto")[0].childNodes[0].nodeValue.substr(0,400) + "..."+"</p>"+ "<a class='btn btn-primary' href='postagem.html?codigo_postagem="+i+"'>Saiba mais</a>"+
+        "<p class='card-text'>"+y[i].getElementsByTagName("texto")[0].childNodes[0].nodeValue.substr(0,400) + "..."+"</p>"+ "<a class='btn btn-primary' href='postagem.html?codigo_postagem="+i+"'class='text-white fw-bold'>Saiba mais</a>"+
+         "</div>"+
+         "</div>"+
         "</div>"+
         "</div>");
    }
 }
 
-function postagem(){
-    //armazena a url completa
+function titulo(){
     url = new URL (window.location.href);
     parametro = url.searchParams;
-    // Armazena a id
     i = parametro.get("codigo_conteudo");
     document.write(
         "<div class='row d-flex px-2 my-4'>" +
           "<div id='title-text-color' class='d-flex col-6 col-md-12 justify-content-center h2'>" +
-          xml_postagem_blog[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +
+          y[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue +
           "</div>" +
         "</div>" +
   
         "<div class='container'>" +
-           "<img class='img-responsive rounded' src='imgs/" + xml_postagem_blog[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue + "' width='100%'>" +
+           "<img class='img-responsive rounded' src='imgs/" + y[i].getElementsByTagName("imagem")[0].childNodes[0].nodeValue + "' width='100%'>" +
            "<br><br><br><br>" +
-           "<a class='h4 text-decoration-none text-light'>" + xml_postagem_blog[i].getElementsByTagName("texto")[0].childNodes[0].nodeValue + "</a>" +
+           "<a class='h4 text-decoration-none text-light'>" + y[i].getElementsByTagName("texto")[0].childNodes[0].nodeValue + "</a>" +
         "</div>"
     );
 }
